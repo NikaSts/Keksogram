d303d684119fddcb5fc85a8297830538cd4893b3
Nefila <veronika.sts@gmail.com>
2020-02-28 15:35:48 +0100

    добавила прерывание цикла

--- js/main.js
+++ js/main.js
@@ -304,17 +304,22 @@ var checkHashtagsInputValidity = function (hashtags) {
 
     for (var i = 0; i < hashtags.length; i++) {
       var hashtag = hashtags[i];
-
+      console.log(i);
       if (hashtag[0] !== '#') {
         hashtagsInput.setCustomValidity('хэш-тег должен начинаться с символа # (решётка)');
+        break;
       } else if (hashtag.length < MIN_HASHTAG_LENGTH) {
         hashtagsInput.setCustomValidity('хеш-тег не может состоять только из одной решётки');
+        break;
       } else if (hashtag.length > MAX_HASHTAG_LENGTH) {
         hashtagsInput.setCustomValidity('максимальная длина одного хэш-тега 20 символов, включая решётку');
+        break;
       } else if (!hashtag.substring(1).match(/^([A-Za-zА-ЯЁа-яё0-9]*)$/)) {
         hashtagsInput.setCustomValidity('название хэш-тега должно состоять только из букв и цифр');
+        break;
       } else if (hashtags.indexOf(hashtag, i + 1) !== -1) {
         hashtagsInput.setCustomValidity('один и тот же хэш-тег не может быть использован дважды');
+        break;
       } else {
         hashtagsInput.setCustomValidity('');
       }
